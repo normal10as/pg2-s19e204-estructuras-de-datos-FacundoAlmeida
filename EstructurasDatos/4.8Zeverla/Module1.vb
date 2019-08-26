4ab434377c7c5f6768d2b28f87820b4b321b2063
facundoa.almeida@outlook.es <facundoa.almeida@outlook.es>
2019-08-26 15:09:11 -0300

    .

--- EstructurasDatos/4.8Zeverla/Module1.vb
+++ EstructurasDatos/4.8Zeverla/Module1.vb
@@ -2,7 +2,20 @@
 
 
     Sub Main()
-
+        Dim Frase As New Stack
+        Dim palabra As String
+        Dim contador As Int16
+        Console.WriteLine("Ingrese una frase, cada palabra apretar enter y punto para finalizar")
+        Do
+            contador = contador + 1
+            palabra = Console.ReadLine()
+            Frase.Push(palabra)
+        Loop Until (palabra = ".")
+        Console.WriteLine("La palabra ingresada es: ")
+        While contador <> 0
+            Console.Write(Frase.Pop & " ")
+            contador = contador - 1
+        End While
 
     End Sub
 
